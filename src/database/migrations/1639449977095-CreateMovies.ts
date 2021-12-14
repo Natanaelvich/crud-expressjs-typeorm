db7c0c6965d53f9cd8556d629cebd1b00bac0cce
Natanael Lima <taelima1997@gmail.com>
2021-12-13 23:55:48 -0300

    add movies migration

---
+++ src/database/migrations/1639449977095-CreateMovies.ts
@@ -0,0 +1,58 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateMovies1639449977095 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "movies",
+
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "category_id",
+            type: "uuid",
+          },
+          {
+            name: "duration",
+            type: "numeric",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "fk_movies_categories",
+            columnNames: ["category_id"],
+            referencedTableName: "categories",
+            referencedColumnNames: ["id"],
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("movies");
+  }
+}
