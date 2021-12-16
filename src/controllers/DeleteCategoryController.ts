171d66840fc84e2b4a8ab4dba691374e628fd243
Natanael Lima <taelima1997@gmail.com>
2021-12-15 23:50:38 -0300

    add DeleteCategoryController

---
+++ src/controllers/DeleteCategoryController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { DeleteCategoryService } from "../services/DeleteCategoryService";
+
+export class DeleteCategoryController {
+  async handle(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const service = new DeleteCategoryService();
+
+    const result = await service.execute({ id});
+
+    if(result instanceof Error){
+        return response.status(404).json({message : result.message})
+    }
+
+    return response.send()
+  }
+}
