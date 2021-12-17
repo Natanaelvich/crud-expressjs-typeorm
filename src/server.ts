96bb20ac98f396b71a5b514434b936ac1588bb07
Natanael Lima <taelima1997@gmail.com>
2021-12-16 23:59:23 -0300

    update server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,28 @@
-import 'reflect-metadata'
+import "reflect-metadata";
 
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 
-import './database'
-import routes from './routes';
+import "./database";
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
-app.use(routes)
+app.use(routes);
+
+app.use((err: Error, _: Request, response: Response, __: NextFunction) => {
+  // if (err instanceof AppError) {
+  //   return response.status(err.statusCode).json({
+  //     status: 'error',
+  //     message: err.message,
+  //   });
+  // }
+
+  return response.status(500).json({
+    status: "error",
+    message: `Internal server error: ${err.message}`,
+  });
+});
+
 app.listen(3000, () => console.log("Server is running"));
