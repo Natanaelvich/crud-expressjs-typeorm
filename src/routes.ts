a8d4ebd26e3c9ba0c9d6ab5e57b17a2558b52b30
Natanael Lima <taelima1997@gmail.com>
2021-12-16 23:59:18 -0300

    update reoutes

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
+import { CreateMovieController } from "./controllers/CreateMovieController";
 import { DeleteCategoryController } from "./controllers/DeleteCategoryController";
 import { GetAllCategoriesController } from "./controllers/GetAllCategoriesController";
+import { GetAllMoviesController } from "./controllers/GetAllMoviesController";
+import { UpdateCategoryController } from "./controllers/UpdateCategoryController";
 
 const routes = Router();
 
 routes.get("/category", new GetAllCategoriesController().handle);
 routes.post("/category", new CreateCategoryController().handle);
+routes.put("/category/:id", new UpdateCategoryController().handle);
 routes.delete("/category/:id", new DeleteCategoryController().handle);
+
+routes.get("/movie", new GetAllMoviesController().handle);
+routes.post("/movie", new CreateMovieController().handle);
 
 export default routes;
