c87ed148f4970fdb733d98b86af538f2ca609758
Natanael Lima <taelima1997@gmail.com>
2021-12-16 23:58:02 -0300

    remove type unused

--- src/services/GetAllCategoriesService.ts
+++ src/services/GetAllCategoriesService.ts
@@ -1,11 +1,6 @@
 import { getRepository } from "typeorm";
 import { Category } from "../entities/Category";
 
-type CategoryRequest = {
-  name: string;
-  description: string;
-};
-
 export class GetAllCategoriesService {
   async execute() {
     const repo = getRepository(Category);
